db34d42c154f5f4b9d6a2769ce0790c2d39f3103
somaia-khalil <sumaya_khair@live.com>
2020-02-16 22:11:46 -0600

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do 
+      erb :root
+    end 
 
+    get '/new' do
+      erb :"pirates/new"
+    end 
+
+    post '/pirates' do
+    @pirate = Pirate.new(params[:pirate])
+     @ship = []
+     params[:pirate][:ships].each do |ship|
+      @ship << Ship.new(ship)
+    end 
+    erb :"pirates/show"
+    end
   end
 end
